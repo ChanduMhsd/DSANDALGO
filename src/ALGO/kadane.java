76f3e8dcfa8b18d4b5669799fdc60a8d01795a26
chandumhsd <mhsdchandu1306@gmail.com>
2023-10-23 00:59:30 +0530

    LL

--- src/ALGO/kadane.java
+++ src/ALGO/kadane.java
@@ -10,21 +10,23 @@ public class kadane {
                 + maxSubArraySum(a));
     }
 
-    // Function Call
+
     static int maxSubArraySum(int a[])
     {
-        int size = a.length;
-        int max_so_far = Integer.MIN_VALUE, max_ending_here
-                = 0;
 
-        for (int i = 0; i < size; i++) {
-            max_ending_here = max_ending_here + a[i];
-            if (max_so_far < max_ending_here)
-                max_so_far = max_ending_here;
-            if (max_ending_here < 0)
-                max_ending_here = 0;
+
+        int max= Integer.MIN_VALUE;
+        int sum=0;
+
+        for (int i = 0; i < a.length; i++) {
+          sum=sum+a[i];
+
+          max=Math.max(sum,max);
+
+          if (sum<0)
+              sum=0;
         }
-        return max_so_far;
+        return max;
     }
 }
 
