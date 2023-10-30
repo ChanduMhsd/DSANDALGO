433d9cbab073d891ab4b8c4a97f33984fcb2e815
chandumhsd <mhsdchandu1306@gmail.com>
2023-10-30 23:13:30 +0530

    LL

---
+++ src/ALGO/Main.java
@@ -0,0 +1,22 @@
+package ALGO;
+
+public class Main {
+    public static void main(String[] args) {
+
+    DLList list=new DLList();
+    list.insertAtStart(3);
+    list.insertAtStart(2);
+    list.insertAtStart(1);
+    list.insertAtLast(4);
+    list.insert(22,3);
+    list.display();
+
+//
+//      List lst=new List();
+//      lst.insertAtStart(2);
+//      lst.insertAtStart(2);
+//      lst.insertAtStart(2);
+//
+//      lst.display();
+    }
+}
